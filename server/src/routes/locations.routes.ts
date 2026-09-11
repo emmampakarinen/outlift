66c79451790447933806e3a71c07c71e71550404
emmampakarinen <emma.m.pakarinen@gmail.com>
2026-09-11 18:27:47 +0300

    post and delete location

--- server/src/routes/locations.routes.ts
+++ server/src/routes/locations.routes.ts
@@ -1,5 +1,6 @@
 import { type Request, type Response, Router } from "express";
 import pool from "../db/db.js";
+import type { Location } from "#shared/types.js";
 
 const locationRouter: Router = Router();
 
@@ -26,6 +27,44 @@ locationRouter.get("/:id", async (req: Request, res: Response) => {
     res.json(location.rows[0]);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch location" });
+  }
+});
+
+locationRouter.post("/", async (req: Request, res: Response) => {
+  const { name, latitude, longitude, description, created_by } =
+    req.body as Location;
+
+  try {
+    const newLocation = await pool.query(
+      `INSERT INTO locations
+       (name, latitude, longitude, description, created_by)
+       VALUES ($1, $2, $3, $4, $5)
+       RETURNING *`,
+      [name, latitude, longitude, description, created_by],
+    );
+
+    res.status(201).json(newLocation.rows[0]);
+  } catch (error) {
+    console.error("Failed to fetch locations:", error);
+    res.status(500).json({ error: "Failed to add new location" });
+  }
+});
+
+locationRouter.delete("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    const result = await pool.query(`DELETE FROM locations WHERE id = $1`, [
+      id,
+    ]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Requested location not found" });
+    }
+    res.status(204).send();
+  } catch (error) {
+    console.error("Failed to delete location:", error);
+    res.status(500).json("Failed to delete location");
   }
 });
 
---
+++ server/src/shared/types.ts
@@ -0,0 +1,9 @@
+export interface Location {
+  id?: Number;
+  name: string;
+  description: string;
+  latitude: number;
+  longitude: number;
+  created_by: number;
+  created_at?: Date;
+}
